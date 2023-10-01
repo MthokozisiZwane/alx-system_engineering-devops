764723348a87d611b031b8b8766fc71cd5b86f10
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-09-30 17:09:14 -0700

    Processes and Signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - creates an infinite loop
+ *
+ * Return: Always returns 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i;
+	pid_t child_pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+
+	infinite_while();
+	return (0);
+}
