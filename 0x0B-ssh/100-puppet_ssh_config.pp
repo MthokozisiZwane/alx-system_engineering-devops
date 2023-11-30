eb89c7eaa3fa486271234a9116dea19138efbe41
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-11-30 09:24:11 -0800

    puppet manipulation

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # using puppet to make changes to configuration file
 
-file { 'ect/ssh/ssh_config':
+file { 'ect/ssh/ssh_cofig':
 	ensure => present,
+
 content =>"
 	
 	#ssh client confuguration
 	host*
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
-	"
+	",
 }
