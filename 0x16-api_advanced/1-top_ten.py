f1806588c983852564d00b38829e678853c55943
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-01-17 11:02:57 -0800

    first 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+querries reddit api and returns top 10 posts titles
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
+    """
+    headers = {'User-Agent': 'my_user_agent'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {}).get('children', [])
+        for post in data:
+            print(post.get('data', {}).get('title'))
+    else:
+        print(None)
+
+
+if __name__ == "__main__":
+    pass
