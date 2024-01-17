e45f5537269c2a02b9f8d06357585708abdbe0f4
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-01-17 10:50:21 -0800

    get number of subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A function that querries thereddit api and
+returns the number of subscribers for
+a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    headers = {'User-Agent': 'my_user_agent'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        return data.get('subscribers', 0)
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    pass
