7ac329e52b580bca4d86d1b2dba6eb2df3124f11
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-01-17 12:17:32 -0800

    api adva

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-querries reddit api and returns top 10 posts titles
+1-top_ten
 """
 import requests
 
@@ -10,7 +10,9 @@ def top_ten(subreddit):
     Prints the titles of the first 10 hot posts for a given subreddit.
     If the subreddit is invalid, prints None.
     """
-    headers = {'User-Agent': 'my_user_agent'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110\
+            Safari/537.3'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     params = {'limit': 10}
     response = requests.get(url, headers=headers, params=params,
