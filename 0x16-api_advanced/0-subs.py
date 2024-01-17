2af38f010b2cc1d2d8d0ba5c4714b553429a66d7
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-01-17 11:55:00 -0800

    ad api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,11 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
-    headers = {'User-Agent': 'my_user_agent'}
+    headers = {
+            'User-Agent': 'RedditAPIRequest/1.0 (+https://github.\
+                    com/MthokozisiZwane)'
+            }
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
 
