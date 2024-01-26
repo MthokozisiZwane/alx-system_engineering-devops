a1d297d894b1acea983cf32daf6b41f6500082e9
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-01-26 14:34:22 -0800

    fixing requests on nginx using puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
-# Fix worker_connections in Nginx
-exec { 'fix-worker-connections':
-  command => 'sed -i "s/worker_connections.*/worker_connections 1024;/g" /etc/nginx/nginx.conf',
-  path    => '/usr/local/bin/:/bin/',
-}
-
 # extendss request limit to nginx
+
 exec { 'fix-request-limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
